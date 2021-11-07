4ffd48ed050f490b6688d3d93f6ff8f2863abeee
nabe-yu <yudai.tanabe0528@gmail.com>
2021-11-07 12:58:19 +0900

    prototype

---
+++ components/Header.vue
@@ -0,0 +1,16 @@
+<template>
+  <section class="hero is-primary is-small">
+    <div class="hero-body">
+      <h1 class="title is-4">
+        Search Office
+      </h1>
+      <h2 class="subtitle is-6">
+        最適なサテライトオフィスを提案します。
+      </h2>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {}
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,182 @@
+/* eslint-disable no-console */
+<template>
+  <section class="section">
+    <div class="content box">
+      <h2 class="title is-5">
+        ファイルをアップロード
+      </h2>
+      <b-field class="file is-primary" :class="{ 'has-name': !!staff_data_file }">
+        <b-upload v-model="staff_data_file" class="file-label" drag-drop>
+          <span class="file-cta">
+            <b-icon class="file-icon" icon="upload" />
+            <span class="file-label">個人情報</span>
+          </span>
+          <span v-if="staff_data_file" class="file-name">
+            {{ staff_data_file.name }}
+          </span>
+        </b-upload>
+      </b-field>
+      {{ staffs }}
+      <b-field class="file is-primary" :class="{ 'has-name': !!office_data_file }">
+        <b-upload v-model="office_data_file" class="file-label" drag-drop>
+          <span class="file-cta">
+            <b-icon class="file-icon" icon="upload" />
+            <span class="file-label">オフィス情報</span>
+          </span>
+          <span v-if="office_data_file" class="file-name">
+            {{ office_data_file.name }}
+          </span>
+        </b-upload>
+      </b-field>
+      {{ offices }}
+    </div>
+    <div v-if="office_data_file && staff_data_file" class="section">
+      <b-button class="is-large" type="is-danger" expanded outlined @click="upload()">
+        ファイルを読み込む
+      </b-button>
+    </div>
+    <div class="section">
+      <b-button
+        v-if="isUpload"
+        class="is-large"
+        type="is-danger"
+        expanded
+        outlined
+        @click="btn()"
+      >
+        計算する
+      </b-button>
+    </div>
+    <b-table v-if="data" :data="data" :columns="columns" />
+  </section>
+</template>
+
+<script>
+import distance from '@turf/distance'
+import { point } from '@turf/helpers'
+export default {
+  data() {
+    return {
+      offices: [],
+      staffs: [],
+      // data: [
+      //   {
+      //     id: '',
+      //     home_address: '',
+      //     current_office_name: '',
+      //     current_office_distance: '',
+      //     satellite_office_name: '',
+      //     satellite_office_distance: '',
+      //     reduced_distance: ''
+      //   }
+      // ],
+      data: [],
+      isUpload: '',
+      staff_data_file: null,
+      office_data_file: null,
+      columns: [
+        {
+          field: 'id',
+          label: 'ID'
+        },
+        {
+          field: 'home_address',
+          label: '自宅'
+        },
+        {
+          field: 'current_office_name',
+          label: '現在の勤務地'
+        },
+        {
+          field: 'current_office_distance',
+          label: '現在の勤務地までの距離'
+        },
+        {
+          field: 'satellite_office_name',
+          label: '最寄りオフィス'
+        },
+        {
+          field: 'satellite_office_distance',
+          label: '最寄りオフィスまでの距離'
+        },
+        {
+          field: 'reduced_distance',
+          label: '削減された距離'
+        }
+      ]
+    }
+  },
+  computed: {},
+  methods: {
+    upload() {
+      this.uploadStaffData()
+      this.uploadOfficeData()
+      this.isUpload = true
+    },
+    async btn() {
+      this.data = []
+      for (const staff of this.staffs) {
+        const currentOffice = this.offices.find(office => office.id === staff.office_id)
+        const currentOfficeCoordinates = await this.getCoordinates(currentOffice.office_address)
+        const homeCoordinates = await this.getCoordinates(staff.home_address)
+        const currentOfficeDistance = this.getDistance([homeCoordinates.coordinates[0], homeCoordinates.coordinates[1]], [currentOfficeCoordinates.coordinates[0], currentOfficeCoordinates.coordinates[1]])
+        const resltList = []
+        for (const office of this.offices) {
+          const officeCoordinates = await this.getCoordinates(office.office_address)
+          resltList.push({
+            name: office.office_name,
+            dis: this.getDistance([homeCoordinates.coordinates[0], homeCoordinates.coordinates[1]], [officeCoordinates.coordinates[0], officeCoordinates.coordinates[1]])
+          })
+        }
+        // eslint-disable-next-line array-callback-return
+        const res = resltList.sort((a, b) => {
+          if (a.dis < b.dis) {
+            return -1
+          };
+        })
+        const resobj = {}
+        resobj.id = staff.id
+        resobj.home_address = staff.home_address
+        resobj.satellite_office_name = res[1].name
+        resobj.current_office_distance = currentOfficeDistance
+        resobj.current_office_name = currentOffice.office_name
+        resobj.satellite_office_distance = res[1].dis
+        resobj.reduced_distance = (Math.round((currentOfficeDistance - res[1].dis) * 100)) / 100
+        this.data.push(resobj)
+      }
+    },
+    uploadOfficeData() {
+      this.offices = ''
+      const officeJson = this.office_data_file
+      const reader = new FileReader()
+      const loadJson = () => {
+        this.offices = JSON.parse(reader.result).offices
+      }
+      reader.onload = loadJson
+      return reader.readAsText(officeJson)
+    },
+    uploadStaffData() {
+      this.staffs = ''
+      const staffJson = this.staff_data_file
+      const reader = new FileReader()
+      const loadJson = () => {
+        this.staffs = JSON.parse(reader.result).staffs
+      }
+      reader.onload = loadJson
+      return reader.readAsText(staffJson)
+    },
+    getDistance(point1, point2) {
+      const result = distance(point(point1), point(point2), { units: 'kilometers' })
+      const roundResult = (Math.round(result * 100)) / 100
+      return roundResult
+    },
+    async getCoordinates(address) {
+      const url = `https://msearch.gsi.go.jp/address-search/AddressSearch?q=${address}`
+      const response = await this.$axios.$get(url)
+      return {
+        coordinates: response[0].geometry.coordinates
+      }
+    }
+  }
+}
+</script>
